5d91eff6f4dd165af1c4bcb222e2b3ce1e3329e4
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-06-27 11:18:07 +0100

    #1.0.2 MySQL Connection foundations

--- src/main/java/technology/otis/example/Example.java
+++ src/main/java/technology/otis/example/Example.java
@@ -1,24 +1,44 @@
 package technology.otis.example;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import technology.otis.example.commands.ReloadCommand;
+import technology.otis.example.db.MySQL;
+
+import java.sql.SQLException;
 
 public final class Example extends JavaPlugin {
     private static Example instance;
-
+    public MySQL sql;
     @Override
     public void onEnable() {
         // Plugin startup logic
         instance = this;
         saveDefaultConfig();
         getCommand("reloadexample").setExecutor(new ReloadCommand());
+        this.sql = new MySQL();
+        try {
+            sql.connect();
+        } catch (ClassNotFoundException | SQLException e ) {
+            Bukkit.getLogger().info("Database is not connected, please update config.yml, check your connection and then try again.");
+            getServer().getPluginManager().disablePlugin(this);
+            getPluginLoader().disablePlugin(this);
+            return;
+        }
+
+        if(sql.isConnected()){
+            getServer().getLogger().info("MySQL Database and Example Plugin are connected!");
+        }
         getServer().getLogger().info("Example Plugin Successfully Enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        getServer().getLogger().info("Examole Plugin Successfully Disabled");
+        getServer().getLogger().info("Example Plugin Successfully Disabled");
+        if(sql.isConnected()) {
+            sql.disconnect();
+        }
     }
 
     public static Example getInstance() {
---
+++ src/main/java/technology/otis/example/db/MySQL.java
@@ -0,0 +1,52 @@
+package technology.otis.example.db;
+
+import technology.otis.example.Example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQL {
+    private String host;
+    private String port;
+    private String database;
+    private String username;
+    private String password;
+    private Connection connection;
+
+    public MySQL() {
+        Example plugin = Example.getInstance();
+        this.host = plugin.getConfig().getString("mysql.host");
+        this.port = plugin.getConfig().getString("mysql.port");;
+        this.database = plugin.getConfig().getString("mysql.database");;
+        this.username = plugin.getConfig().getString("mysql.username");;
+        this.password = plugin.getConfig().getString("mysql.password");;
+    }
+
+    public Boolean isConnected() {
+        return (connection == null ? false : true);
+
+    }
+
+    public void connect() throws ClassNotFoundException, SQLException {
+        if(isConnected()) return;
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false",
+                username, password);
+    }
+
+    public void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
+
+
+
